72d521d60faa0d682383bdce01c128b0251d1215
Sreeram0045 <sreeramjmenon10@gmail.com.com>
2024-07-27 19:19:02 +0530

    Add:Added a feature to check whether the start point and end point are the same

--- api/GetBusDetails.php
+++ api/GetBusDetails.php
@@ -1,5 +1,10 @@
 <?php
     if(isset($_POST["submit_start_and_end"])){
         echo "Got the value";
+        // echo $_POST["start_point"];
+        // echo $_POST["end_point"];
+        if ($_POST["start_point"] === $_POST["end_point"]){
+            die("The start point and the end point are the same no services can be found");
+        }
     }
 ?>
