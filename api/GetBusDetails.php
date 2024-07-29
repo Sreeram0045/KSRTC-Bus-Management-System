6c8454340a0a8266746442a4455cfa5e3cf70da3
Sreeram0045 <sreeramjmenon10@gmail.com.com>
2024-07-29 21:54:37 +0530

    feat:Added search based on service type

--- api/GetBusDetails.php
+++ api/GetBusDetails.php
@@ -1,6 +1,7 @@
 <?php
 require_once "../connection.php";
-if(isset($_POST["submit_start_and_end"])) {
+
+if (isset($_POST["submit_start_and_end"])) {
     // Check if the starting point and end point are the same
     if ($_POST["start_point"] === $_POST["end_point"]) {
         die("The start point and the end point are the same. No services can be found.");
@@ -8,6 +9,7 @@ if(isset($_POST["submit_start_and_end"])) {
 
     $starting = $_POST["start_point"];
     $ending = $_POST["end_point"];
+    $service_type = $_POST["service_type"];
 
     $query = "SELECT 
                 b.bus_id,
@@ -42,25 +44,37 @@ if(isset($_POST["submit_start_and_end"])) {
                 st_start.station_name = ? 
                 AND st_end.station_name = ?
                 AND so_start.stop_order < so_end.stop_order";
-    
+
+    // Add service type filter if specified
+    if (!empty($service_type)) {
+        $query .= " AND stype.service_name = ?";
+    }
+
     $stmt = $connection->prepare($query);
-    $stmt->bind_param("ss", $starting, $ending);
-    $stmt->execute();   
+    
+    if (!empty($service_type)) {
+        $stmt->bind_param("sss", $starting, $ending, $service_type);
+    } else {
+        $stmt->bind_param("ss", $starting, $ending);
+    }
+
+    $stmt->execute();
     $result = $stmt->get_result();
 
     if ($result->num_rows > 0) {
         echo "<table>";
-            echo "<tr>";
-            echo "<th>Bus ID</th>";
-            echo "<th>Service</th>";
-            echo "<th>Pickup Point</th>";
-            echo "<th>Scheduled Reaching Time</th>";
-            echo "<th>Drop-Off Point</th>";
-            echo "<th>Scheduled Dropping Time</th>";
-            echo "<th>Current Status</th>";
-            echo "<th>Current Delay</th>";
-            echo "</tr>";
-        while($row = $result->fetch_assoc()) {
+        echo "<tr>";
+        echo "<th>Bus ID</th>";
+        echo "<th>Service</th>";
+        echo "<th>Pickup Point</th>";
+        echo "<th>Scheduled Reaching Time</th>";
+        echo "<th>Drop-Off Point</th>";
+        echo "<th>Scheduled Dropping Time</th>";
+        echo "<th>Current Status</th>";
+        echo "<th>Current Delay</th>";
+        echo "</tr>";
+
+        while ($row = $result->fetch_assoc()) {
             $bus_id = $row['bus_id'];
             $start_scheduled_time = new DateTime($row['start_scheduled_time']);
             $end_scheduled_time = new DateTime($row['end_scheduled_time']);
@@ -72,52 +86,50 @@ if(isset($_POST["submit_start_and_end"])) {
             $status = $row['state'];
             $service_name = $row['service_name'];
 
-            if(!$total_no_of_stops===2){
-            // average time taken between each stops in hours and minutes
-            // echo $avg_time_taken."<br>";
-            // converting the average time taken between each stops to minutes
-            [$avg_hours,$avg_minutes]=explode(":",$avg_time_taken);
-            $total_avg_minutes=(int)$avg_hours*60+(int)$avg_minutes;
-            // echo $total_avg_minutes."<br>";
+            if ($total_no_of_stops !== 2) {
+                // Average time taken between each stop in hours and minutes
+                [$avg_hours, $avg_minutes] = explode(":", $avg_time_taken);
+                $total_avg_minutes = (int)$avg_hours * 60 + (int)$avg_minutes;
 
-            // recalibarating the number of stop for calculation
-            $start_order=$start_order-1;
-            $end_order=$end_order-1;
-            
-            // Calculating start and end times
-            $adjusted_start_time = clone $start_scheduled_time;
-            $adjusted_start_time->modify('+' . ($start_order * $total_avg_minutes) . ' minutes');
-            
-            $adjusted_end_time = clone $start_scheduled_time;
-            $adjusted_end_time->modify('+' . ($end_order * $total_avg_minutes) . ' minutes');
-            // small h ie;(h) denotes the conversion from 24 hrs to 12 hrs and A at the end indicates the AM and PM
-            echo "<tr>";
-            echo "<td>". $bus_id . "</td>";
-            echo "<td>". $service_name . "</td>";
-            echo "<td>". $starting . "</td>";
-            echo "<td>". $adjusted_start_time->format('h:i:sA') . "</td>";
-            echo "<td>". $ending . "</td>";
-            echo "<td>". $adjusted_end_time->format('h:i:sA') . "</td>";
-            echo "<td>". $status . "</td>";
-            echo "<td>". $delay . " Min</td>";
-            echo "</tr>";
-            }else{
+                // Recalibrating the number of stops for calculation
+                $start_order -= 1;
+                $end_order -= 1;
+
+                // Calculating start and end times
+                $adjusted_start_time = clone $start_scheduled_time;
+                $adjusted_start_time->modify('+' . ($start_order * $total_avg_minutes) . ' minutes');
+
+                $adjusted_end_time = clone $start_scheduled_time;
+                $adjusted_end_time->modify('+' . ($end_order * $total_avg_minutes) . ' minutes');
+
+                // Small h denotes the conversion from 24 hrs to 12 hrs and A at the end indicates AM and PM
                 echo "<tr>";
-                echo "<td>". $bus_id . "</td>";
-                echo "<td>". $service_name . "</td>";
-                echo "<td>". $starting . "</td>";
-                echo "<td>". $start_scheduled_time->format('h:i:sA') . "</td>";
-                echo "<td>". $ending . "</td>";
-                echo "<td>". $end_scheduled_time->format('h:i:sA') . "</td>";
-                echo "<td>". $status . "</td>";
-                echo "<td>". $delay . " Min</td>";
+                echo "<td>" . $bus_id . "</td>";
+                echo "<td>" . $service_name . "</td>";
+                echo "<td>" . $starting . "</td>";
+                echo "<td>" . $adjusted_start_time->format('h:i:sA') . "</td>";
+                echo "<td>" . $ending . "</td>";
+                echo "<td>" . $adjusted_end_time->format('h:i:sA') . "</td>";
+                echo "<td>" . $status . "</td>";
+                echo "<td>" . $delay . " Min</td>";
+                echo "</tr>";
+            } else {
+                echo "<tr>";
+                echo "<td>" . $bus_id . "</td>";
+                echo "<td>" . $service_name . "</td>";
+                echo "<td>" . $starting . "</td>";
+                echo "<td>" . $start_scheduled_time->format('h:i:sA') . "</td>";
+                echo "<td>" . $ending . "</td>";
+                echo "<td>" . $end_scheduled_time->format('h:i:sA') . "</td>";
+                echo "<td>" . $status . "</td>";
+                echo "<td>" . $delay . " Min</td>";
                 echo "</tr>";
             }
         }
+
         echo "</table>";
     } else {
         echo "No results found.";
     }
 }
-
 ?>
